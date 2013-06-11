6635030e91e2c849f80a8cd8af36f9b4e1e4a156
Daniel <d.horbach@arcor.de>
2013-06-11 09:39:25 +0200

    Grundgeruest gebaut und Gems eingefuegt

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 FancyForum::Application.routes.draw do
+  
+  root :to => 'home#index'
+  get "home/index"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -45,10 +49,6 @@ FancyForum::Application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-
-  # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
 
   # See how all your routes lay out with "rake routes"
 
