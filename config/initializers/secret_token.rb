b0be615777c3ef15e0271790b164bca48c592d94
Daniel <d.horbach@arcor.de>
2013-06-11 08:52:06 +0200

    Erster Commit der Railsapp mit eigener Ignorefile

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FancyForum::Application.config.secret_token = 'd815a973c45579f857d1d0980d14c9a347aa592cd8d5f2ff34e22ea332d336bec78c5841935bdd82333ec497608187f29940f3fa46c23902c250300cc314f713'
