3a03aab462e91365de72df2648cf1cd983372450
Daniel <d.horbach@arcor.de>
2013-06-12 11:41:27 +0200

    Breadcrumbs eingefuegt

--- app/controllers/forum_controller.rb
+++ app/controllers/forum_controller.rb
@@ -2,4 +2,8 @@ class ForumController < ApplicationController
   def topic
     @topic = Topic.find(params[:id])
   end
+  
+  def category
+    @topics = Category.find(params[:id]).topics
+  end
 end
