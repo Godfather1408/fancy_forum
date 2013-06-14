5cf31ebb8c8abd1322a5c5b45f23e663eba661e4
Daniel <d.horbach@arcor.de>
2013-06-14 10:50:56 +0200

    Code aufgeraeumt, man kann jetzt neue topics in der Uebersicht anlegen

--- app/helpers/forum_helper.rb
+++ app/helpers/forum_helper.rb
@@ -2,4 +2,8 @@ module ForumHelper
   def posts
     @topic.posts.order('created_at ASC').paginate( :page => params[:page], :per_page =>25)
   end
+  
+  def last_post_from_user?(post)
+    @topic.posts.order('created_at DESC').where(:user_id => current_user.id).first.id == post.id
+  end
 end
