9f28ee20dcfe6b8bc2f2f8932ff18a8f788cb04f
dhorbach148 <dhorbach@avarteq.de>
2013-07-23 09:49:46 +0200

    Favorite hinzugefuegt

--- spec/models/favorite_spec.rb
+++ spec/models/favorite_spec.rb
@@ -1,5 +1,15 @@
 require 'spec_helper'
 
 describe Favorite do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before { 
+    @category = Category.new(title: "hello", description: "hup hup")
+    @topic = Topic.new(title: "Tiel yeah!!", description: "Einfach so", category_id: @category.id, important: true)
+    @user = User.new(email: "dh@test.de", last_name: "lname", first_name: "fname", username: "uname")
+    @favorite = Favorite.new(topic_id: @topic.id, user_id: @user.id)
+  }
+  
+  subject { @favorite }
+  it { should respond_to(:topic_id) }
+  it { should respond_to(:user_id) }
+  
 end
